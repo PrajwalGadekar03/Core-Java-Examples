3dc8df4527ec79d0b39d10266f2b07059356b10a
Prajwal Gadekar <126348429+PrajwalGadekar03@users.noreply.github.com>
2024-03-06 14:43:59 +0530

    Made a changes in Decision_Making_Statement package

---
+++ src/Decision_Making_Statements/If01.java
@@ -0,0 +1,16 @@
+package Decision_Making_Statements;
+
+public class If01 {
+	public static void main(String[] args) {
+		
+			String study = "12th";
+			if (study == "12th")
+			{
+				System.out.println("Study first year!");
+			}
+			System.out.println("Study second year!");
+			System.out.println("Study third year!");
+			System.out.println("Study fourth year!");
+
+}
+}
